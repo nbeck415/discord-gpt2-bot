681b6172ab4180f6ffbcf9aca15644e82451fffc
nbeck415 <natalielunbeck@yahoo.com>
2020-06-10 14:42:53 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,15 @@
 import discord
 import random
 import simple_text_gen
-#print(discord.__version__)  # check to make sure at least once you're on the right version!
 
-#token = open("token.txt", "r").read()  # I've opted to just save my token to a text file.
 generator = simple_text_gen.SimpleTextGen("reddit_comments.txt", 50)
-token = "NzIwMzY5ODg1OTI1ODY3NTgw.XuE_OA.m9zXl4dLScmfnuWdTBFSEmH0oWE"
 
 client = discord.Client()  # starts the discord client.
 
 
-@client.event  # event decorator/wrapper. More on decorators here: https://pythonprogramming.net/decorators-intermediate-python-tutorial/
-async def on_ready():  # method expected by client. This runs once when connected
-    print(f'We have logged in as {client.user}')  # notification of login.
+@client.event
+async def on_ready(): 
+    print(f'We have logged in as {client.user}')
 
 
 @client.event
@@ -21,10 +18,10 @@ async def on_message(message):
     chance = random.randrange(10)
     #responds by chance or by summon
     if chance == 3 or message.content.startswith("&talk"):
-        userinput = message.content.replace("&talk","")
+        userinput = message.content.replace("&talk ","")
         generator.talk(userinput)
         response = open("bot_says.txt","r").read()
         print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
-        await client.send_message(message.channel, response)
+        await message.channel.send(response)
 
-client.run(token)  # recall my token was saved!
+client.run(token)
