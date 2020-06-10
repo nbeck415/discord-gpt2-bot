3948f393518ae621fe3691a14471316f2a7944d8
nbeck415 <natalielunbeck@yahoo.com>
2020-06-10 13:57:32 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,15 @@
 import discord
 import random
 import simple_text_gen
-#print(discord.__version__)  # check to make sure at least once you're on the right version!
 
-#token = open("token.txt", "r").read()  # I've opted to just save my token to a text file.
 generator = simple_text_gen.SimpleTextGen("reddit_comments.txt", 50)
 token = myTokenHere
 client = discord.Client()  # starts the discord client.
 
 
-@client.event  # event decorator/wrapper. More on decorators here: https://pythonprogramming.net/decorators-intermediate-python-tutorial/
-async def on_ready():  # method expected by client. This runs once when connected
-    print(f'We have logged in as {client.user}')  # notification of login.
+@client.event 
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
 
 @client.event
